80ef200fdf4ae320b1f700f77abeff8db8900b12
Jeff Tian <jeff.tian@outlook.com>
2022-03-11 16:54:42 +0800

    fix: imports

--- src/main/java/com/example/keycloakspringboot/DemoApplication.java
+++ src/main/java/com/example/keycloakspringboot/DemoApplication.java
@@ -2,9 +2,8 @@ package com.example.keycloakspringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.*;
 
 @Controller
 @SpringBootApplication
