eefcf895a4004356f9c01280fc927cbe71291173
Jeff Tian <jeff.tian@outlook.com>
2022-03-11 19:10:21 +0800

    feat: Integrate with Keycloak

--- src/main/java/com/example/keycloakspringboot/DemoApplication.java
+++ src/main/java/com/example/keycloakspringboot/DemoApplication.java
@@ -15,6 +15,13 @@ public class DemoApplication {
 		return "Hello Keycloak!";
 	}
 
+	@RequestMapping("/visitor")
+	@ResponseBody
+	public String getVisitorPath() {
+		return "嗨，你好！当你看到这些文字，说明你成功登录了！";
+	}
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
