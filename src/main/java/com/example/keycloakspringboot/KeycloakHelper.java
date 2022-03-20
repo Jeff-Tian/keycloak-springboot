d1c4bc572e0d1fa5e21243b188f101fc7418e53e
Jeff Tian@Workstation <jie.tian@live.cn>
2022-03-20 19:34:44 +0800

    refactor: extract jsonRequest

--- src/main/java/com/example/keycloakspringboot/KeycloakHelper.java
+++ src/main/java/com/example/keycloakspringboot/KeycloakHelper.java
@@ -3,6 +3,7 @@ package com.example.keycloakspringboot;
 import com.example.keycloakspringboot.models.KeycloakAccessTokenPayload;
 import com.example.keycloakspringboot.models.UserPayload;
 import okhttp3.*;
+import org.bouncycastle.cert.ocsp.Req;
 
 import java.io.IOException;
 import java.util.Objects;
@@ -15,48 +16,36 @@ public class KeycloakHelper {
                 .build();
     }
 
-    public String createUser(UserPayload user) throws IOException {
-        var accessToken = this.getAdminAccessToken().access_token;
-
+    public Response jsonRequest(String url, String payload) {
         var mediaType = MediaType.parse("application/json");
-        var body = RequestBody.create(mediaType, "{\"firstName\":\"Sergey\",\"lastName\":\"Kargopolov\", \"email\":\"" + user.getEmail() + "\", \"enabled\":\"true\", \"username\":\"" + user.getUsername() + "\", \"credentials\":[{\"type\":\"password\",\"value\":\"" + user.getPassword() + "\",\"temporary\":false}]}");
-        var request = new Request.Builder()
-                .url("https://keycloak.jiwai.win/auth/admin/realms/UniHeart/users")
-                .method("POST", body)
-                .addHeader("Content-Type", "application/json")
-                .addHeader("Authorization", "Bearer " + accessToken)
-                .build();
-        var createUserResponse = client.newCall(request).execute();
-        return createUserResponse.toString();
+        var body = RequestBody.create(mediaType, payload);
+        var request = new Request.Builder().url(url).method("POST", body).addHeader("Content-Type", "application/json").addHeader("Authorization", getAdminAccessToken().access_token).build();
+        return client.newCall(request).execute();
+    }
+
+    public String createUser(UserPayload user) throws IOException {
+        var url = "https://keycloak.jiwai.win/auth/admin/realms/UniHeart/users";
+        var payload = java.lang.String.format("{\"firstName\":\"Sergey\",\"lastName\":\"Kargopolov\", \"email\":\"%s\", \"enabled\":\"true\", \"username\":\"%s\", \"credentials\":[{\"type\":\"password\",\"value\":\"%s\",\"temporary\":false}]}", user.getEmail(), user.getUsername(), user.getPassword());
+        return this.jsonRequest(url, payload).toString();
     }
 
     public String assignRole(String userId) {
-        var accessToken = this.getAdminAccessToken().access_token;
-        var mediaType = MediaType.parse("application/json");
-
         var clientId = "98ea8f07-a7f2-4607-ab56-b5208a90eaa1";
-
-        var body = RequestBody.create(mediaType, "[{\"id\": \"bef4bf69-371b-460a-8a0c-b2943da1983b\"," +
+        var url = java.lang.String.format("https://keycloak.jiwai.win/auth/admin/realms/UniHeart/users/%s/role-mappings/clients/%s", userId, clientId);
+        var payload = "[{\"id\": \"bef4bf69-371b-460a-8a0c-b2943da1983b\"," +
                 "\"name\":\"visitor\",\"description\":\"add roles programatically\",\"composite\":false," +
-                "\"clientRole\":true,\"containerId\":\"" + clientId + "\"}]");
-
-        var request =
-                new Request.Buidler().url("https://keycloak.jiwai.win/auth/admin/realms/UniHeart/users/" + userId +
-                                "/role-mappings/clients/" + clientId).method("POST", body)
-                        .addHeader("Content-Type", "application/json")
-                        .addHeader("Authorization", "Bearer " + accessToken)
-                        .build();
-        return (client.newCall(request).execute()).toString();
+                "\"clientRole\":true,\"containerId\":\"" + clientId + "\"}]";
+        return this.jsonRequest(url, payload).toString();
     }
 
     public KeycloakAccessTokenPayload getAdminAccessToken() throws IOException {
         var username = System.getenv("KC_ADMIN");
         var password = System.getenv("KC_PASSWORD");
 
-        System.out.println("username = " + username + "; password = " + password);
+        System.out.println(java.lang.String.format("username = %s; password = %s", username, password));
 
         var mediaType = MediaType.parse("application/x-www-form-urlencoded");
-        var body = RequestBody.create(mediaType, "username=" + username + "&password=" + password + "&grant_type=password&client_id=admin-cli");
+        var body = RequestBody.create(mediaType, java.lang.String.format("username=%s&password=%s&grant_type=password&client_id=admin-cli", username, password));
         var request = new Request.Builder()
                 .url("https://keycloak.jiwai.win/auth/realms/master/protocol/openid-connect/token")
                 .method("POST", body)
