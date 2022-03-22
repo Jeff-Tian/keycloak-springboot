f0e43117d75b7097fc6eae6303f82dc3cfc48e5e
Jeff Tian@Workstation <jie.tian@live.cn>
2022-03-22 12:21:28 +0800

    fix: use realm admin api

--- src/main/java/com/example/keycloakspringboot/KeycloakHelper.java
+++ src/main/java/com/example/keycloakspringboot/KeycloakHelper.java
@@ -68,7 +68,8 @@ public class KeycloakHelper {
     public java.lang.String assignRealmRole(String userId) throws IOException {
         System.out.println(java.lang.String.format("assigning realm role for user = %s", userId));
         var clientId = "98ea8f07-a7f2-4607-ab56-b5208a90eaa1";
-        var url = java.lang.String.format("https://keycloak.jiwai.win/auth/admin/realms/UniHeart/users/%s/role-mappings/clients/%s", userId, clientId);
+        var url = java.lang.String.format("https://keycloak.jiwai.win/auth/admin/realms/UniHeart/users/%s/role" +
+                "-mappings/realm", userId);
         var payload = java.lang.String.format("[{\"id\": \"bef4bf69-371b-460a-8a0c-b2943da1983b\"," +
                 "\"name\":\"Offline_access\",\"description\":\"add roles programatically\",\"composite\":false," +
                 "\"clientRole\":false,\"containerId\":\"%s\"}]", clientId);
